5add6366ea33d835b00a08e39294a11acf51438a
Yasser Azán Basallo <yazanenator@gmail.com>
2020-08-14 00:59:07 -0500

    agregando clase d prueba y preparando para publicar  en heroku

---
+++ tests/Feature/APITest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class APITest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testAPIGuzzle(){
+
+    }
+}
